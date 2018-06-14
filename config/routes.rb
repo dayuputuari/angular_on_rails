5575ca6b811ffa865370b26a7ea280502b6aa264
Ida Ayu Ari <dayu.crish@gmail.com>
2018-06-14 14:48:34 +0800

    AngularJS on Rails app - Events Manager

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  root 'dashboard#index'
+
+  namespace :api do
+    resources :events, only: [:index, :create, :update, :destroy] do
+    	collection do
+    		get :search
+    	end
+    end
+  end
+end
